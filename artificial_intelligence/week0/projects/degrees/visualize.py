0c304de5aff45e8019d2993cc21f791fa046f0bd
Daniel Barton <daniel.barton446@gmail.com>
2024-03-02 20:19:35 -0500

    feat: slightly better implementation

--- artificial_intelligence/week0/projects/degrees/visualize.py
+++ artificial_intelligence/week0/projects/degrees/visualize.py
@@ -2,44 +2,60 @@ import argparse
 import csv
 
 from typing import List
+from collections import OrderedDict
 
-# Person
-row_keys = []
-# Movie
-col_keys = []
-
-
-def visualize_graph(graph: List[List[int]]):
+def visualize_graph(graph: List[List[int]], row_mappings: OrderedDict):
+    row_labels = list(row_mappings.keys())
     for i, row in enumerate(graph):
-        print(f'{row} - {row_keys[i]}')
+        print(f'{row} - {row_labels[i]}')
 
 
-def build_graph(dir: str) -> List[List[int]]:
-    graph: List[List[int]] = [[0 for col in col_keys] for row in row_keys]
+def build_graph(dir: str, row_mappings: OrderedDict,
+                          col_mappings: OrderedDict) -> List[List[int]]:
+    row_size = len(row_mappings.keys())
+    col_size = len(col_mappings.keys())
+    graph: List[List[int]] = [[0 for col in range(0, col_size)] for row in range(0, row_size)]
     with open(f'{dir}/stars.csv', 'r') as mapping:
         reader = csv.DictReader(mapping)
         for record in reader:
-            row = row_keys.index(record['person_id'])
-            col = col_keys.index(record['movie_id'])
+            # These two "index" calls are O(n) operations
+            row = row_mappings.get(record['person_id'])
+            col = col_mappings.get(record['movie_id'])
+            if row is None or col is None:
+                raise ValueError(f'Row or Col not found for {record}')
             graph[row][col] = 1
+    print("Finished Building Graph")
 
     return graph
 
 
-def set_keys(dir: str):
+def build_dimensions(dir: str) -> (OrderedDict, OrderedDict):
     # this will open the people.csv and movies.csv
     # file and just read the id columns of both and set
     # our rows and cols
     # This is basically the translation of
     # the array IDX to the actual ids.
+
+    # Person_id -> graph_idx
+    row_mappings = OrderedDict()
+    # Movie_id -> graph_idx
+    col_mappings = OrderedDict()
+
+
     with open(f'{dir}/movies.csv', 'r') as movies_csv:
         reader = csv.DictReader(movies_csv)
+        idx = 0
         for row in reader:
-            col_keys.append(row['id'])
+            col_mappings[row['id']] = idx
+            idx += 1
     with open(f'{dir}/people.csv', 'r') as people_csv:
         reader = csv.DictReader(people_csv)
+        idx = 0
         for row in reader:
-            row_keys.append(row['id'])
+            row_mappings[row['id']] = idx
+            idx += 1
+    print(f"Finished Building Dimensions: {len(col_mappings)}, {len(row_mappings)}")
+    return (row_mappings, col_mappings)
 
 
 def main():
@@ -47,9 +63,9 @@ def main():
     arg_parser.add_argument('dir', type=str, help='path to the people and movie directory')
 
     args = arg_parser.parse_args()
-    set_keys(args.dir)
-    graph = build_graph(args.dir)
-    visualize_graph(graph)
+    (row_mappings, col_mappings) = build_dimensions(args.dir)
+    graph = build_graph(args.dir, row_mappings, col_mappings)
+    visualize_graph(graph, row_mappings)
 
 
 if __name__ == "__main__":
